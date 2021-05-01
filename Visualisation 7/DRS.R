e5f1059e946fae8b6cfae8b551fabaaa6ccfaf27
Akmal Rafiq <akyrafiq@gmail.com>
2021-05-01 14:39:28 +0100

    DRS tidy graph

--- Visualisation 7/DRS.R
+++ Visualisation 7/DRS.R
@@ -10,10 +10,25 @@ setwd("~/Documents/GitHub/data_viz_2021/Visualisation 7")
 #read in data
 reviews<-read.csv("drs_tests.csv")
 
-ump<-reviews %>% count(Umpire,Result) 
+reviews[reviews$Umpire=="RA Kattleborough ",]$Umpire<-"RA Kettleborough"
+ump<-reviews %>%
+     count(Umpire,Result) %>%
+     left_join(reviews %>%
+               count(Umpire) %>%
+               rename(Games=n)) %>% 
+     mutate(Prop=n/Games) %>% 
+     rename(Num=n)
 
-ggplot(ump,aes(x=Umpire,y=n,fill=Result))+geom_col()+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggplot(ump %>% filter(Games>20),aes(x=reorder(reorder(Umpire,Result),-Num),y=Prop*100,fill=Result))+
+  geom_col()+
+  theme_calc()+
+  ylim(0,100)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  xlab("Umpire")+
+  ylab("Probability")+
+  labs(title="Which umpires fair best against DRS?", 
+       subtitle="The outcome of reviews against various umpires (Min. 20 reviews)")
+  
 
 ggsave("umpires.png")
 
